2eaf53ffcb6ba462d706899fb1f5242df9583da2
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-01-10 22:26:08 +0100

    Removed folder

--- c++ primer/ex_1_6.cpp
+++
@@ -1,5 +0,0 @@
-/* std::cout << "The sum of " << v1; 
-    << " and " << v2;
-    << " is " << v1 + v2 << std::endl; */
-
-//Program fragment is not legal because the << operator is missing it's left hand opperand on lines 2 and 3
